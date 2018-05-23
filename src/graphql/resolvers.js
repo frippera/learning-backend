fc4177168a4213caabe195bf710251e3f71155d3
Anders Friborg <frippera@gmail.com>
2018-05-23 12:13:46 +0200

    Fixed user query

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -6,41 +6,41 @@ import createToken from './createToken';
 import { RegisterError, LoginError } from './errorHandler';
 
 const resolvers = {
-  Query: {
-    users: () => User.find({}),
-    user: (id) => User.findOne({ id }),
-  },
-  Mutation: {
-    register: async (parent, { name, email, password }) => {
-      const newUser = { name, email, password };
-      newUser.password = await bcrypt.hash(newUser.password, 12);
+	Query: {
+		users: () => User.find({}),
+		user: (_, { id }) => User.findOne({ _id: id })
+	},
+	Mutation: {
+		register: async (parent, { name, email, password }) => {
+			const newUser = { name, email, password };
+			newUser.password = await bcrypt.hash(newUser.password, 12);
 
-      const userExists = await User.findOne({ email });
-      if(userExists) {
-        throw new RegisterError();
-      }
+			const userExists = await User.findOne({ email });
+			if (userExists) {
+				throw new RegisterError();
+			}
 
-      const user = await User(newUser).save();
-      const token = await createToken(user);
+			const user = await User(newUser).save();
+			const token = await createToken(user);
 
-      return { token, user }; 
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-      if(!user) {
-        throw new LoginError();
-      }
+			return { token, user };
+		},
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+			if (!user) {
+				throw new LoginError();
+			}
 
-      const validPassword = await bcrypt.compare(password, user.password);
-      if(!validPassword) {
-        throw new LoginError();
-      }
+			const validPassword = await bcrypt.compare(password, user.password);
+			if (!validPassword) {
+				throw new LoginError();
+			}
 
-      const token = await createToken(user);
+			const token = await createToken(user);
 
-      return { token, user };
-    },
-  },
-}
+			return { token, user };
+		}
+	}
+};
 
 export default resolvers;
