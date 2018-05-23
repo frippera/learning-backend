ca04f5a7f9f61fd309dff370ed0ac53ee8fd2a18
Anders Friborg <frippera@gmail.com>
2018-05-23 09:46:28 +0200

    Fixed build problem

--- src/server.js
+++ src/server.js
@@ -1,27 +1,29 @@
-import { GraphQLServer } from 'graphql-yoga'
 import 'babel-polyfill';
+import { GraphQLServer } from 'graphql-yoga';
+import { importSchema } from 'graphql-import';
+
 import mongoose from 'mongoose';
-import { formatError } from "apollo-errors";
+import { formatError } from 'apollo-errors';
 import dotenv from 'dotenv';
-dotenv.load()
+dotenv.load();
 
 import resolvers from './graphql/resolvers';
-import typeDefs from './graphql/schema.graphql';
+const typeDefs = importSchema('./src/graphql/schema.graphql');
 
 const connection = mongoose.connect(process.env.DB);
 
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers
-})
+	typeDefs,
+	resolvers
+});
 
-const serverOptions = { 
-  formatError
-}
+const serverOptions = {
+	formatError
+};
 
 /*
 // TODO Add middelware to check for auth (token) and pass to resolvers
 const validToken = jwt.verify(token, process.env.SECRET);
 */
 
-server.start(serverOptions, () => console.log(`Server is running`))
+server.start(serverOptions, () => console.log(`Server is running`));
